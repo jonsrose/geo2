e89202f4600d1805ffab162555fe216d38871f0e
Jon Rose <jon@jonrose.com>
2016-04-07 15:08:34 -0700

    added react map stuff

--- containers/Root.dev.js
+++ containers/Root.dev.js
@@ -6,12 +6,12 @@ import { Router } from 'react-router'
 export default class Root extends Component {
   render() {
     const { store, history } = this.props
-    console.log(`/containers/Root.dev.js Root.render() store=`)
+    console.log('/containers/Root.dev.js Root.render()')
     console.log(store)
     return (
       <Provider store={store}>
         <div>
-          {console.log(`/containers/Root.dev.js Root.render() inside provider store=`)}
+          {console.log('/containers/Root.dev.js Root.render() inside provider store=')}
           {console.log(store)}
           <Router history={history} routes={routes} />
         </div>
