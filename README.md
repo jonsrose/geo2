60fd9ed1d7ef0b51d81c37fe5d31d632342f7e31
Jon Rose <jon@jonrose.com>
2016-05-09 15:40:52 -0700

    took out some more unused code, modified read

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-TODO: fix the issue of selecting location, then going back to map, then going back to location
-TODO: add images description
+# GEOJUMP
+
+GEOJUMP helps you discover new places completely by random.
+
+It generates random coordinates via latitude and longitude then you jump to those coordinates on a map.
+
+It then searches nearby for wikipedia entries or flickr photos nearby. If they are found it will display them in the left nav.
+
+If wikipedia locations are found close by, they will be listed in the side nav or as markers on the page, red markers are for wikipedia, blue markers are for flickr. The user can click on the item in the side nav or on the marker to display the entry.
+
+User can touch anywhere on the map to jump to a another nearby location on the map, and it will scan for nearby places based on the new location.
+
+The app uses Reactjs and the Redux library. It is based off of the real world example in the redux library. It uses React Router to modify the url and provide browser history and bookmarking support. 
--- routes.js
+++ routes.js
@@ -10,14 +10,10 @@ import FlickrPhotoPage from './containers/FlickrPhotoPage'
 export default (
   <Route path="/" component={App}>
     <IndexRoute components={{leftChildren: LeftNavMain, rightChildren: MapPage}}/>
-    <Route path="/coordinates/:coordinatesString"
-         components={{leftChildren: LeftNavMain, rightChildren: MapPage}}/>
-    <Route path="/coordinates/:coordinatesString/placeDetail"
-         components={{leftChildren: PlaceDetail, rightChildren: MapPage}}>
-      <Route path="/coordinates/:coordinatesString/placeDetail/localityInfo/:locality"
-          component={LocalityPage}/>
-        <Route path="/coordinates/:coordinatesString/placeDetail/flickrPhoto/:flickrPhotoId"
-        component={FlickrPhotoPage}/>
+    <Route path="/coordinates/:coordinatesString" components={{leftChildren: LeftNavMain, rightChildren: MapPage}}/>
+    <Route path="/coordinates/:coordinatesString/placeDetail" components={{leftChildren: PlaceDetail, rightChildren: MapPage}}>
+      <Route path="/coordinates/:coordinatesString/placeDetail/localityInfo/:locality" component={LocalityPage}/>
+      <Route path="/coordinates/:coordinatesString/placeDetail/flickrPhoto/:flickrPhotoId" component={FlickrPhotoPage}/>
     </Route>
   </Route>
 )
