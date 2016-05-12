744e7acde067d0567e4cade45f16c180731bd74f
Jon Rose <jon@jonrose.com>
2016-05-11 22:07:06 -0700

    uglify in production

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,8 @@ var webpack = require('webpack')
 
 var env = process.env.NODE_ENV
 
+var prod = process.env.NODE_ENV === 'production'
+
 module.exports = {
   devtool: 'source-map',
   entry: [
@@ -14,13 +16,23 @@ module.exports = {
     filename: 'bundle.js',
     publicPath: '/static/'
   },
-  plugins: [
+  plugins: prod
+  ? [
     new webpack.optimize.OccurenceOrderPlugin(),
     new webpack.HotModuleReplacementPlugin(),
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify(env)
-    })
-  ],
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+    compress: { warnings: false }
+    })]
+  : [
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(env)
+    })]
+  ,
   module: {
     loaders: [
       {
